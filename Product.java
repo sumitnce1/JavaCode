093917f3946a329c0f76fcab22005b96584449ed
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-07-04 19:52:45 +0530

    product.java
    Assign two number calculate and display product and difference

---
+++ Product.java
@@ -0,0 +1,10 @@
+/* Assign two number calculate and display product and difference */
+class Product
+{
+	public static void main(String arg[])
+	{
+		int a=10, b=6;
+		System.out.println("The value of product: "+(a*b));
+		System.out.print("The value of difference: " +(a-b));
+	}
+}

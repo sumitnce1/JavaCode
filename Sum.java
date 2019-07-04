39b207edb5ab1f01fbf620ac0d8056a888c2c0b8
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-07-04 19:51:57 +0530

    Sum.java
    WAP in java to assign two round integer, calculate and display sum and average.

---
+++ Sum.java
@@ -0,0 +1,12 @@
+/* WAP in java to assign two round integer, calculate and display sum and average. */
+class Sum
+{	
+	public static void main(String art[])
+	{
+		int a=5, b=10;
+		System.out.println("a="+a+",b="+b);
+		System.out.println("Sum="+a+b);
+		System.out.println("Sum="+(a+b));
+		System.out.println("Averagte="+(a+b)/2.0);
+	}
+}

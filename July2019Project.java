ea452af984e4b99d7e50634c6bc1a3629e7850fe
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-08-02 19:25:28 +0530

    Add files via upload

---
+++ July2019Project.java
@@ -0,0 +1,8 @@
+package july2019project;
+public class July2019Project {
+    public static void main(String[] args) {
+        System.out.print("welcome to java");
+        Number x=new Number();
+        x.disp();
+    }
+}

1190f188621a45568fe6cd59eabfb73c7afd6bfe
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-08-05 07:48:08 +0530

    Add files via upload

---
+++ FrameDemo3.java
@@ -0,0 +1,13 @@
+class FrameDemo3 extends java.awt.Frame
+{
+	public FrameDemo3()
+	{
+		setVisible(true);
+		setSize(400,300);
+		setTitle("This is third frame");
+	}
+	public static void main(String arg[])
+	{
+		FrameDemo3 f1=new FrameDemo3();
+	}
+}
